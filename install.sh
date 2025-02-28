435a7977f135125770e5d662214a6fbd59257069
k4ttitude <pmh.ars14@gmail.com>
2025-03-01 00:20:21 +0700

    feat: stow.sh
    for single package

--- install.sh
+++ install.sh
@@ -3,13 +3,7 @@
 for folder in $(ls -d */ | cut -f1 -d '/'); do
   echo ""
   echo "stow $folder"
-  if [ -e "$folder/.target" ]; then
-    target_expression=$(cat "$folder/.target")
-    eval "target=$target_expression"
-  else
-    target=$HOME
-  fi
-  stow -R -v --target="$target" $folder
+  ./stow.sh $folder
 done
 
 echo ""
